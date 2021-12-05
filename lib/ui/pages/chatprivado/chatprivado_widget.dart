f5bf1f696fc93da1ae4d6e7c5add5cd0af786ba6
kaiserbl <juancamilomanrrique34@gmail.com>
2021-12-04 21:54:32 -0500

    cambios para arreglar la app

--- lib/ui/pages/chatprivado/chatprivado_widget.dart
+++ lib/ui/pages/chatprivado/chatprivado_widget.dart
@@ -1,7 +1,5 @@
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ChatprivadoWidget extends StatefulWidget {
   const ChatprivadoWidget({Key key}) : super(key: key);
