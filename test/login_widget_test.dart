e4dc07bd0cf90cdc05ad2a08a2a042c144a19af8
Andres C <cabelloa@uninorte.edu.co>
2021-12-11 15:23:33 -0500

    update

--- test/login_widget_test.dart
+++ test/login_widget_test.dart
@@ -9,4 +9,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:proyecto_mintic/ui/pages/login/login_widget.dart';
 
-void main() {}
+void main() {
+  testWidgets('Email y contraseña vacías no permiten ingreso',
+      (WidgetTester tester) async {
+    //Crea una página de Login
+    LoginWidget loginWidget = new LoginWidget();
+  });
+}
