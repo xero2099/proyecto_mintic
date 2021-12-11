732b54e0704ddd0871fc9180bb92dd94dbf32e21
Andres C <cabelloa@uninorte.edu.co>
2021-12-11 15:29:13 -0500

    update

--- test/login_widget_test.dart
+++ test/login_widget_test.dart
@@ -12,7 +12,14 @@ import 'package:proyecto_mintic/ui/pages/login/login_widget.dart';
 void main() {
   testWidgets('Email y contraseña vacías no permiten ingreso',
       (WidgetTester tester) async {
+    Widget buildTestableWidget(Widget widget) {
+      return new MediaQuery(
+          data: new MediaQueryData(), child: new MaterialApp(home: widget));
+    }
+
     //Crea una página de Login
     LoginWidget loginWidget = new LoginWidget();
+    //Se agrega al tester de widgets
+    await tester.pumpWidget(buildTestableWidget(loginWidget));
   });
 }
