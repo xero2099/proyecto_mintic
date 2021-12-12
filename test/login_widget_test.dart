7950bf7a2d751fb58e640106a62532bd43f49f61
Andres C <cabelloa@uninorte.edu.co>
2021-12-11 19:13:28 -0500

    update

--- test/login_widget_test.dart
+++ test/login_widget_test.dart
@@ -13,15 +13,22 @@ void main() {
   group('Comprobación de funcionalidad de widget de login >', () {
     testWidgets('Email y contraseña vacías no permiten ingreso >',
         (WidgetTester tester) async {
+      print('Inicializando widget');
       Widget buildTestableWidget(Widget widget) {
         return new MediaQuery(
             data: new MediaQueryData(), child: new MaterialApp(home: widget));
       }
 
+      print('Widget inicializado');
+
+      print('Construyendo página de login');
+
       //Crea una página de Login
       LoginWidget loginWidget = new LoginWidget();
       //Se agrega al tester de widgets
       await tester.pumpWidget(buildTestableWidget(loginWidget));
+
+      print('Página de login creada');
 
       //Se presiona en el botón de Login
       Finder loginButton = find.byKey(Key('loginSubmit'));
@@ -30,9 +37,12 @@ void main() {
       //Se 'bombea' el test de nuevo
       await tester.pump();
 
+      print('Comprobando que campos de texto están vacíos');
+
       //Se comprueba que el campo de usuario está vacío
       Finder userText = find.byKey(Key('textUser'));
       expect(userText.toString().contains(''), true);
+      print('Campo de texto de usuario vacío');
 
       //Se 'bombea' el test de nuevo
       await tester.pump();
@@ -40,26 +50,36 @@ void main() {
       //Se comprueba que el campo de contraseña está vacío
       Finder passText = find.byKey(Key('textPass'));
       expect(passText.toString().contains(''), true);
+
+      print('Campo de texto de contraseña vacío');
     });
 
     testWidgets('Contraseña vacía no permite ingreso',
         (WidgetTester tester) async {
+      print('Inicializando widget');
       Widget buildTestableWidget(Widget widget) {
         return new MediaQuery(
             data: new MediaQueryData(), child: new MaterialApp(home: widget));
       }
 
+      print('Widget inicializado');
+
+      print('Construyendo página de login');
       //Crea una página de Login
       LoginWidget loginWidget = new LoginWidget();
+
+      print('Página de login creada');
       //Se agrega al tester de widgets
       await tester.pumpWidget(buildTestableWidget(loginWidget));
 
       Finder userText = find.byKey(Key('textUser'));
       Finder passText = find.byKey(Key('textPass'));
 
+      //Se ingresa un correo electrónico válido
       await tester.enterText(userText, 'lucas@xyz.com');
       await tester.enterText(passText, '');
 
+      //Se presiona el botón de Login
       Finder loginButton = find.byKey(Key('loginSubmit'));
       await tester.tap(loginButton);
     });
